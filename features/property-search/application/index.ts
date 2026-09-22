678bc218526967efb8890422e4492b68dfdaa502
alireza <mirmohamadihayka@gmail.com>
2026-09-22 08:20:33 +0330

    feat: implement property search application use case

---
+++ features/property-search/application/index.ts
@@ -0,0 +1,13 @@
+export {
+  createPropertySearchUseCase,
+  type PropertySearchApplicationError,
+  type PropertySearchApplicationResult,
+  type PropertySearchUseCase,
+} from "@/features/property-search/application/property-search-use-case";
+
+export type {
+  PropertySearchItem,
+  PropertySearchRepository,
+  PropertySearchRepositoryFailure,
+  PropertySearchRepositoryResult,
+} from "@/features/property-search/application/property-search-repository";
